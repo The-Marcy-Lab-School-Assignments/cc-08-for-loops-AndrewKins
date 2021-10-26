5b18f02e528dea8e7a8ed8467a64bea2de1513ec
Andrew Enoe <andrewm.enoe@gmail.com>
2021-10-26 14:36:06 +0000

    solved questions

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 //1
 function countToTwenty(){
-  
+    for(let i=1; i<=20; i++) {
+        console.log(i)
+    }
 }
-
+countToTwenty()
 //2
 function countFromTwenty(){
-  
+for(let i=20; i>=1; i--)
+console.log(i)
 }
-
+countFromTwenty()
 //3
-function countToN(){
-  
+function countToN(n){
+for(let i=1; i<=n; i++)
+console.log(i)
 }
-
+countToN(10)
 
 //4
 function countFromN(){
